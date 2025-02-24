4f1aae4ee006f04a759cb25faa11f708343b9f5a
Ohad <ohadr@mail.tau.ac.il>
2025-02-24 10:00:34 +0200

    ..

---
+++ src/reg_examples/example3.py
@@ -0,0 +1,65 @@
+from src.goap import AutomatonController, RegAction, RegSensor, RegGoal
+from pathlib import Path
+
+class DirectoryStateSensor(RegSensor):
+    binding = "tmp_dir_state"
+
+    def exec(self):
+        tmp_path = Path("/tmp/goap_tmp")
+        out = "exist" if tmp_path.exists() else "not_exist"
+        return out
+
+
+class TokenStateSensor(RegSensor):
+    binding = "tmp_dir_content"
+
+    def exec(self):
+        token_path = Path("/tmp/goap_tmp") / ".token"
+        out = "token_found" if token_path.exists() else "token_not_found"
+        return out
+
+
+class CreateTmpDir(RegAction):
+    effects = {"tmp_dir_state": "exist", "tmp_dir_content": "token_not_found"}
+    preconditions = {"tmp_dir_state": "not_exist", "tmp_dir_content": "token_not_found"}
+
+    def exec(self):
+        Path("/tmp/goap_tmp").mkdir(parents=True, exist_ok=True)
+
+
+class CreateToken(RegAction):
+    effects = {"tmp_dir_state": "exist", "tmp_dir_content": "token_found"}
+    preconditions = {"tmp_dir_state": "exist", "tmp_dir_content": "token_not_found"}
+
+    def exec(self):
+        (Path("/tmp/goap_tmp") / ".token").touch()
+
+
+class CreateTokenGoal(RegGoal):
+    desired_state = {"tmp_dir_state": "exist", "tmp_dir_content": "token_found"}
+    priority = 1
+import fire
+def start():
+    world_state_matrix = {
+        "tmp_dir_state": "Unknown",
+        "tmp_dir_content": "Unknown",
+    }
+    dir_handler = AutomatonController(
+        name="directory_watcher",
+        actions=[CreateTmpDir(), CreateToken()],
+        sensors=[DirectoryStateSensor(), TokenStateSensor()],
+        world_state=world_state_matrix,
+        initial_goal=CreateTokenGoal(),
+    )
+    dir_handler.start()
+
+
+def reset():
+    Path("/tmp/goap_tmp/.token").unlink()
+    Path("/tmp/goap_tmp").rmdir()
+
+
+# usage: python -m reg_examples.example3 start
+# usage: python -m reg_examples.example3 reset
+if __name__ == "__main__":
+    fire.Fire({"start": start, "reset": reset})
